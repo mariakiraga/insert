3ee716bd6cf392dd0a8fb42abf7040a8360670ba
mariakiraga <80244108+mariakiraga@users.noreply.github.com>
2024-05-08 17:27:14 +0200

    snake

--- snake.py
+++ snake.py
@@ -9,7 +9,7 @@ WYSOKOSC_EKRANU = 600
 WIELKOŚĆ_EKRANU = (SZEROKOSC_EKRANU, WYSOKOSC_EKRANU)
 ekran = pygame.display.set_mode(WIELKOŚĆ_EKRANU)
 
-FPS = 5
+FPS = 60
 zegar = pygame.time.Clock()
 
 
@@ -23,13 +23,43 @@ tekstRect = tekst.get_rect(center = (SZEROKOSC_EKRANU/2, WYSOKOSC_EKRANU/2-200))
 # tekst3
 # tekstRect3
 
-
-x = 100
-y = 100
+szer = 20
+x = [100, 100-szer, 100-2*szer, 100-3*szer]
+y = [100, 100, 100, 100]
 kierunek = "prawo"
+
+sciezka = "jablko.png"
+jablko = pygame.image.load(sciezka)
+xj = SZEROKOSC_EKRANU/2
+yj = WYSOKOSC_EKRANU/2
+
+def jedzenie():
+	global xj, yj
+
+	zla_pozycja = True
+
+	while zla_pozycja:
+		xj = random.randint(0, SZEROKOSC_EKRANU / szer - 1) * szer
+		yj = random.randint(0, WYSOKOSC_EKRANU / szer - 1) * szer
+
+		zla_pozycja = False
+
+		for i in range(len(x)-1):
+			if x[i] == xj and y[i] == yj:
+				zla_pozycja = True
+
+def kolizja():
+	global x, y, xj, yj
+
+	if x[0] == xj and y[0] == yj:
+		jedzenie()
+
 
 gramy = True
 game_over = False
+
+klatki = 0
+szybkosc = 3
 
 while gramy:
 	for event in pygame.event.get():
@@ -40,6 +70,7 @@ while gramy:
   
 
 	ekran.fill((255,255,255))
+	ekran.blit(jablko, (xj,yj))
 
 	klawisz = pygame.key.get_pressed()
 	#print(klawisz)
@@ -58,28 +89,38 @@ while gramy:
 		kierunek = "prawo"
 
 
-	if kierunek == "prawo":
-		x = x + 30
-	if kierunek == "lewo":
-		x = x - 30
-	if kierunek == "góra":
-		y = y - 30
-	if kierunek == "dół":
-		y = y + 30
+	
 
 
-	if x <= 0 or x+50 >= SZEROKOSC_EKRANU or y <= 0 or y+50 >= WYSOKOSC_EKRANU:
+	if x[0] <= 0 or x[0]+50 >= SZEROKOSC_EKRANU or y[0] <= 0 or y[0]+50 >= WYSOKOSC_EKRANU:
 		gramy = False
 		game_over = True
 
+	if klatki == szybkosc:
+		for i in range(len(x)-1, 0, -1):
+			x[i] = x[i-1]
+			y[i] = y[i-1]
+
+		if kierunek == "prawo":
+			x[0] = x[0] + 30
+		if kierunek == "lewo":
+			x[0] = x[0] - 30
+		if kierunek == "góra":
+			y[0] = y[0] - 30
+		if kierunek == "dół":
+			y[0] = y[0] + 30
+
+		klatki = 0
+
+	for i in range(len(x)):
+
+		pygame.draw.rect(ekran, (0,0,255), (x[i], y[i], 30, 30)) # (ekran, (kolor), (x, y, dlugosc, szerokosc))
+		
 
 
-
-
-	pygame.draw.rect(ekran, (0,0,255), (x, y, 30, 30)) # (ekran, (kolor), (x, y, dlugosc, szerokosc))
-
-
-
+	klatki += 1
+	print(klatki)
+	
 	pygame.display.update()
 	zegar.tick(FPS)
 
